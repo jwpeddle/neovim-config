8185181a05c138229e2aa9026734ebc1e557b027
Jason Peddle <jason@typh.com>
2021-12-24 10:27:19 -0500

    Scripted auto-commit on change (2021-12-24 10:27:19) by gitwatch.sh

--- fresh.lua
+++ fresh.lua
@@ -1,13 +1,13 @@
-" Install vim-plug if not found
-if empty(glob('~/.vim/autoload/plug.vim'))
-  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
-    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-endif
-
-" Run PlugInstall if there are missing plugins
-autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
-  \| PlugInstall --sync | source $MYVIMRC
-\| endif
+--" Install vim-plug if not found
+--if empty(glob('~/.vim/autoload/plug.vim'))
+--  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+--    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+--endif
+--
+--" Run PlugInstall if there are missing plugins
+--autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)'))
+--  \| PlugInstall --sync | source $MYVIMRC
+--\| endif
 
 
 " load plugins
