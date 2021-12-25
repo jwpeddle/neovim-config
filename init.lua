3c5fb84e2566ac6682faf46b483770639e8f7b0c
Jason Peddle <jason@typh.com>
2021-12-25 00:28:11 -0500

    Scripted auto-commit on change (2021-12-25 00:28:11) by gitwatch.sh

--- init.lua
+++ init.lua
@@ -111,6 +111,11 @@ wk.register({
     w = { "<cmd>bwipeout<cr>", "Close buffer" },
     W = { "<cmd>%bwipeout<cr>", "Close all buffers" },
     q = { "<cmd>quit<cr>", "Quit" },
+    t = { "<cmd>FloatermToggle<cr>" },
+--nnoremap <silent> <C-Space> :FloatermToggle<CR>
+--tnoremap <silent> <C-Space> <C-\><C-N>:FloatermToggle<CR>
+--"tnoremap <silent> <C-d> <C-\><C-N>:FloatermToggle<CR>
+--tnoremap <silent> <Leader><Esc> <C-\><C-N>
   },
   ["<leader>f"] = {
     name = "+fzf",
