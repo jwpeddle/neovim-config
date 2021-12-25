231117181ef7894607fb3892a961f18e264bedd1
Jason Peddle <jason@typh.com>
2021-12-25 01:25:46 -0500

    Scripted auto-commit on change (2021-12-25 01:25:46) by gitwatch.sh

--- init.lua
+++ init.lua
@@ -12,11 +12,12 @@ require("packer").startup(function(use)
   use({ "wbthomason/packer.nvim" })
   use({ "akinsho/bufferline.nvim" })
   use({ "dracula/vim", as = "dracula" })
+  use([ "ggandor/lightspeed.nvim" })
   use({ "folke/which-key.nvim" })
   use({ "junegunn/fzf", dir = "~/.fzf", run = "./install --all" })
   use({ "junegunn/fzf.vim" })
   use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-  use({ 'voldikss/vim-floaterm' })
+  use({ "voldikss/vim-floaterm" })
 end)
 
 g.python_host_prog = "$HOME/.pyenv/versions/neovim2/bin/python"
