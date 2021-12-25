edd23786c5e624b66057f13efb79e15d10dac282
Jason Peddle <jason@typh.com>
2021-12-24 23:27:44 -0500

    Scripted auto-commit on change (2021-12-24 23:27:44) by gitwatch.sh

--- init.lua
+++ init.lua
@@ -5,8 +5,9 @@ local g = vim.g
 
 local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  print("foobar")
   packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+else
+  print("foobar")
 end
 
 require("packer").startup(function(use)
