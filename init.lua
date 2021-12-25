9a8dbdf4542235743e34c50db8714b346986becf
Jason Peddle <jason@typh.com>
2021-12-24 23:33:31 -0500

    Scripted auto-commit on change (2021-12-24 23:33:31) by gitwatch.sh

--- init.lua
+++ init.lua
@@ -9,6 +9,13 @@ if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 require("packer").startup(function(use)
   use({"wbthomason/packer.nvim"})
   use({"akinsho/bufferline.nvim"})
