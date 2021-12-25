9517c78994b2f5a21f521888588992e0e5c5bfb6
Jason Peddle <jason@typh.com>
2021-12-25 13:59:30 -0500

    Scripted auto-commit on change (2021-12-25 13:59:30) by gitwatch.sh

--- init.lua
+++ init.lua
@@ -141,7 +141,7 @@ wk.register({
       c = { "<Cmd>lua require('telescope.builtin').commands()<CR>", "Commands" },
       h = { "<Cmd>lua require('telescope.builtin').help_tags()<CR>", "Help" },
       g = { "<Cmd>lua require('telescope.builtin').live_grep()<CR>", "Search" },
-      g = { "<Cmd>lua require('telescope.builtin').file_browser()<CR>", "File browser" },
+      e = { "<Cmd>lua require('telescope.builtin').file_browser()<CR>", "File browser" },
   },
   ["<Leader><Leader>"] = {
     name = "+prefix",
