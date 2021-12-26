a0f6775beda07351b8105cb4bc28e9b9f869fb22
Jason Peddle <jason@typh.com>
2021-12-26 00:08:40 -0500

    Scripted auto-commit on change (2021-12-26 00:08:40) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -88,7 +88,9 @@ require("packer").startup(function(use)
       --terminal mode
       wk.register({
         ["<C-Space>"] = { "<Cmd>FloatermToggle<CR>", "Close terminal" },
-      }, { mode = "t" })
+      }, {
+        mode = "t",
+      })
     end
   })
 
