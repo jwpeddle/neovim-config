e3f724cdcfdfd7690ee1f7bfe993a5821716d199
Jason Peddle <jason@typh.com>
2021-12-26 01:58:16 -0500

    Scripted auto-commit on change (2021-12-26 01:58:16) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -76,6 +76,7 @@ require("packer").startup(function(use)
         ["<Leader>f"] = {
           name = "+Telescope",
           f = { "<Cmd>lua require('telescope.builtin').find_files()<CR>", "Files" },
+          F = { "<Cmd>lua require('telescope.builtin').builtins()<CR>", "Files" },
           b = { "<Cmd>lua require('telescope.builtin').buffers()<CR>", "Buffers" },
           c = { "<Cmd>lua require('telescope.builtin').commands()<CR>", "Commands" },
           h = { "<Cmd>lua require('telescope.builtin').help_tags()<CR>", "Help" },
