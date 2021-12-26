73f1d07e1492f82291c3c468a6df8fdd877e551f
Jason Peddle <jason@typh.com>
2021-12-26 11:45:47 -0500

    Scripted auto-commit on change (2021-12-26 11:45:47) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -180,6 +180,8 @@ require("packer").startup(function(use)
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
     }
   })
 
