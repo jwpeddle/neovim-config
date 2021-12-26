66636561d53722a878827f70d0c71a79ce39cb44
Jason Peddle <jason@typh.com>
2021-12-26 15:33:20 -0500

    Scripted auto-commit on change (2021-12-26 15:33:20) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -194,7 +194,7 @@ require("packer").startup(function(use)
           end,
         },
         completion = {
-          completeopt = { "menu", "menuone", "noselect" },
+          completeopt = "menu,menuone,noinsert" },
         },
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
