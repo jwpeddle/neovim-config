ee6e2c33d682e9d76ac9e16a1a83102b935d6576
Jason Peddle <jason@typh.com>
2021-12-30 22:41:33 -0500

    Scripted auto-commit on change (2021-12-30 22:41:33) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -135,7 +135,7 @@ require("packer").startup(function(use)
           python = {
             analysis = {
               typeCheckingMode = "off",
-            }
+            },
           }
         }
       })
