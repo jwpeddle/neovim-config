6efe2b1af4ec2b57680cea78c7dada9c55b03573
Jason Peddle <jason@typh.com>
2022-01-24 14:11:27 -0500

    Scripted auto-commit on change (2022-01-24 14:11:27) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -147,7 +147,10 @@ require("packer").startup(function(use)
     "kyazdani42/nvim-tree.lua",
     requires = {
       "kyazdani42/nvim-web-devicons",
-    }
+    },
+    config = function()
+      require("nvim.tree").setup({})
+    end
   })
 
   --sandwich
