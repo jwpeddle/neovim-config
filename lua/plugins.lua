eda16f72d61efde44c0c92627daab3b90dcd7fb9
Jason Peddle <jason@typh.com>
2022-01-31 17:10:54 -0500

    Scripted auto-commit on change (2022-01-31 17:10:54) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -98,9 +98,9 @@ require("packer").startup(function(use)
 
       -- Setup lspconfig.
       local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-      -- require("lspconfig")["pyright"].setup {
-      --   capabilities = capabilities,
-      -- }
+      require("lspconfig")["pyright"].setup {
+        capabilities = capabilities,
+      }
     end
   })
 
