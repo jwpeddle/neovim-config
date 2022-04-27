22c8ab1486b7be20cc6546ccf2828d3e2174496f
Jason Peddle <jason@typh.com>
2022-04-27 17:17:48 -0400

    Scripted auto-commit on change (2022-04-27 17:17:48) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -131,6 +131,7 @@ require("packer").startup(function(use)
         capabilities = capabilities,
         settings = {
           pylsp = {
+            configurationSources = { "flake8", "mypy" }
             plugins = {
               flake8 = { enabled = true },
               mypy = { enabled = true },
