72ea1395950ce2ab515de33d93cc425b769457b6
Jason Peddle <jason@typh.com>
2023-02-09 15:01:51 -0500

    Scripted auto-commit on change (2023-02-09 15:01:51) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -277,7 +277,7 @@ require("packer").startup(function(use)
           W = { "<Cmd>%bwipeout<CR>", "Close all buffers" },
           q = { "<Cmd>quit<cr>", "Quit" },
           t = { "<C-Space>", "Open terminal" },
-          ["<Tab>"] = { "<Cmd> lua require('cmp').complete({config={sources={{name='copilot'}}}})<CR>", "Copilot" },
+          ["<Tab>"] = { "<Cmd>lua require('cmp').complete({config={sources={{name='copilot'}}}})<CR>", "Copilot" },
         },
         --lists
         ["<Leader>d"] = {
