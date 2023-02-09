d18c1757b4d5b0b26bc57ad9ae37b14f70bce7a5
Jason Peddle <jason@typh.com>
2023-02-09 15:06:05 -0500

    Scripted auto-commit on change (2023-02-09 15:06:05) by gitwatch.sh

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -109,7 +109,7 @@ require("packer").startup(function(use)
               fallback()
             end
           end, { "i", "s" }),
-          ["<C-Tab>"] = cmp.mapping(function(fallback)
+          ["  "] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif has_words_before() then
