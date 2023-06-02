a486816ae91e8a75251d3f1d7d3d07b508fba55e
Jason Peddle <jason@typh.com>
2023-06-02 19:13:56 -0400

    Scripted auto-commit on change (2023-06-02 19:13:56) by gitwatch.sh

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,7 +5,18 @@ return {
       require("lspconfig").pylsp.setup({
         settings = {
           pylsp = {
+            configurationSources = { "flake8" },
             plugins = {
+              autopep8 = { enabled = false },
+              flake8 = { enabled = false },
+              pydocstyle = { enabled = false },
+              pycodestyle = { enabled = false },
+              pyflakes = { enabled = false },
+              pylint = { enabled = false },
+              pylsp_mypy = { enabled = false, dmypy = true },
+              rope_completion = { enabled = false },
+              rope_rename = { enabled = false },
+              yapf = { enabled = false },
               ruff = { enabled = true, },
             }
           }
