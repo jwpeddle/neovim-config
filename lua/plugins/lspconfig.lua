0cac63a81864e50e103a07991735d7e7876266f3
Jason Peddle <jason@typh.com>
2023-06-02 19:14:12 -0400

    Scripted auto-commit on change (2023-06-02 19:14:12) by gitwatch.sh

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -13,7 +13,6 @@ return {
               pycodestyle = { enabled = false },
               pyflakes = { enabled = false },
               pylint = { enabled = false },
-              pylsp_mypy = { enabled = false, dmypy = true },
               rope_completion = { enabled = false },
               rope_rename = { enabled = false },
               yapf = { enabled = false },
