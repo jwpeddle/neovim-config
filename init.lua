af1e2610b11654b70fa20ae1586a679f7be9dcb0
Jason Peddle <jason@typh.com>
2023-06-02 13:36:31 -0400

    Scripted auto-commit on change (2023-06-02 13:36:31) by gitwatch.sh

--- init.lua
+++ init.lua
@@ -97,6 +97,11 @@ require("lazy").setup(
   "plugins",
   {
     ui = {
+      custom_keys = {
+        ["<localleader>d"] = function(plugin)
+          vim.pretty_print(plugin)
+        end,
+      },
     },
   }
 )
