684c15cc42c7b5b36ed114b580109953bb5a7134
Jason Peddle <jason@typh.com>
2023-06-02 15:49:19 -0400

    Scripted auto-commit on change (2023-06-02 15:49:19) by gitwatch.sh

--- init.lua
+++ init.lua
@@ -1,6 +1,24 @@
 vim.g.python_host_prog = "$HOME/.pyenv/versions/neovim2/bin/python"
 vim.g.python3_host_prog = "$HOME/.pyenv/versions/neovim3/bin/python"
 
+
+--******************** bootstrap plugins ********************
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+
 --******************** options ********************
 
 --theme
@@ -78,22 +96,6 @@ vim.cmd([[
 --\ too hard to press
 vim.g.mapleader = " " 
 
-
---plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
 
 --******************** which-key ********************
 
