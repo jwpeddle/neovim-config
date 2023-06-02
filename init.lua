847042a330eba8f2cf9bc9a69a3835e262559d62
Jason Peddle <jason@typh.com>
2023-06-02 18:54:54 -0400

    Scripted auto-commit on change (2023-06-02 18:54:54) by gitwatch.sh

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-vim.g.python_host_prog = "$HOME/.pyenv/versions/neovim2/bin/python"
-vim.g.python3_host_prog = "$HOME/.pyenv/versions/neovim3/bin/python"
-
-
 --******************** plugins ********************
 
 --bootstrap lazy
