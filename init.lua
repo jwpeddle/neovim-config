5c5f8d0e46df9cbc2bbdc52bf8f3c1479d9dcae2
Jason Peddle <jason@typh.com>
2023-06-02 19:02:28 -0400

    Scripted auto-commit on change (2023-06-02 19:02:28) by gitwatch.sh

--- init.lua
+++ init.lua
@@ -95,12 +95,6 @@ vim.cmd([[
   augroup end
 ]])
 
---disable provider checkhealth warnings
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_node_provider = 0
-
 
 --******************** which-key ********************
 
