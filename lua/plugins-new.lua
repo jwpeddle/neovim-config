ba6e42a4b0464b80bc4c6f6aeec164fbb5e45197
Jason Peddle <jason@typh.com>
2023-06-02 11:14:50 -0400

    Scripted auto-commit on change (2023-06-02 11:14:49) by gitwatch.sh

--- lua/plugins-new.lua
+++ lua/plugins-new.lua
@@ -1,16 +1,15 @@
---auto-install packer
-local install_path = vim.fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
---recompile packages when changing this file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
+vim.opt.rtp:prepend(lazypath)
 
 require("packer").startup(function(use)
   --aerial - symbol tree nav
