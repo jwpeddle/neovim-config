1491b139bc590695373bb03d1a5bc92c5a497e67
Jason Peddle <jason@typh.com>
2023-06-02 13:09:32 -0400

    Scripted auto-commit on change (2023-06-02 13:09:32) by gitwatch.sh

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,10 +1,12 @@
 return {
-  "akinsho/bufferline.nvim",
-  opts = {
-    options = {
-      show_buffer_icons = false,
-      show_buffer_close_icons = false,
-      show_close_icon = false,
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        show_buffer_icons = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      }
     }
   }
 }
