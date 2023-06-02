1c36d27635862b74ab67d84d3d55851dfb06650f
Jason Peddle <jason@typh.com>
2023-06-02 12:48:26 -0400

    Scripted auto-commit on change (2023-06-02 12:48:26) by gitwatch.sh

--- plugins/aerial.lua
+++ plugins/aerial.lua
@@ -1,16 +1,16 @@
 return {
   {
-  "stevearc/aerial.nvim",
-  opts = {
-    autojump = true,
-    close_on_select = true,
-    post_jump_cmd = "normal! zt",
-    layout = {
-      default_direction = "prefer_left",
-    },
-    on_attach = function(bufnr)
-      require("aerial.actions").tree_close_all.callback()
-    end
+    "stevearc/aerial.nvim",
+    opts = {
+      autojump = true,
+      close_on_select = true,
+      post_jump_cmd = "normal! zt",
+      layout = {
+        default_direction = "prefer_left",
+      },
+      on_attach = function(bufnr)
+        require("aerial.actions").tree_close_all.callback()
+      end
+    }
   }
-}
 }
