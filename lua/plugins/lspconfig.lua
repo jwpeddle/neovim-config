c9fd83e37693dfe3abfbcf9a79a3a37a04bf0ae8
Jason Peddle <jason@typh.com>
2023-06-02 16:31:38 -0400

    Scripted auto-commit on change (2023-06-02 16:31:38) by gitwatch.sh

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,5 +1,27 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    config = function()
+      require("lspconfig").pylsp.setup({
+        settings = {
+          pylsp = {
+            configurationSources = { "flake8" },
+            plugins = {
+              autopep8 = { enabled = false },
+              flake8 = { enabled = false },
+              pydocstyle = { enabled = false },
+              pycodestyle = { enabled = false },
+              pyflakes = { enabled = false },
+              pylint = { enabled = false },
+              pylsp_mypy = { enabled = false, dmypy = true },
+              rope_completion = { enabled = false },
+              rope_rename = { enabled = false },
+              yapf = { enabled = false },
+              ruff = { enabled = true, },
+            }
+          }
+        }
+      })
+    end
   }
 }
