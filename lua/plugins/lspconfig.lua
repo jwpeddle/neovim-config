eb2030dd0a60acbb1848a3d8816ec823fd28232c
Jason Peddle <jason@typh.com>
2023-06-02 19:42:30 -0400

    Scripted auto-commit on change (2023-06-02 19:42:30) by gitwatch.sh

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,14 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("lspconfig").ruff_lsp.setup({
-        init_options = {
-          settings = {
-            -- Any extra CLI arguments for `ruff` go here.
-            args = {},
-          }
-        }
-      })
+      require("lspconfig").ruff_lsp.setup({})
     end
   }
 }
