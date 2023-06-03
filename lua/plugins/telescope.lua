54fe88547fd69fae1f1ea884b9a2a6dc5c4f5943
Jason Peddle <jason@typh.com>
2023-06-02 20:37:41 -0400

    Scripted auto-commit on change (2023-06-02 20:37:41) by gitwatch.sh

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,48 +1,46 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-      requires = {
-        "kyazdani42/nvim-web-devicons",
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-file-browser.nvim",
-        "kkharji/sqlite.lua",
-        "gbprod/yanky.nvim",
-        "debugloop/telescope-undo.nvim",
+    dependencies = {
+      "kyazdani42/nvim-web-devicons",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+      "kkharji/sqlite.lua",
+      "gbprod/yanky.nvim",
+      "debugloop/telescope-undo.nvim",
+    },
+    opts = 
+    defaults = {
+      sorting_strategy = "ascending",
+      layout_config = {
+        height = 0.5,
+        prompt_position = "top",
       },
-      config = function()
-        require("telescope").setup({
-            defaults = {
-            sorting_strategy = "ascending",
-            layout_config = {
-            height = 0.5,
-            prompt_position = "top",
-            },
-            mappings = {
-            i = {
-            ["<esc>"] = require("telescope.actions").close,
-            ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble,
-            },
-            n = {
-            ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble,
-            }
-            },
-            },
-            extensions = {
-            undo = {
-            side_by_side = true,
-            layout_config = {
-              preview_height = 0.8,
-            },
-            },
-            },
-        })
+      mappings = {
+        i = {
+          ["<esc>"] = require("telescope.actions").close,
+          ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble,
+        },
+        n = {
+          ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble,
+        }
+      },
+    },
+    extensions = {
+      undo = {
+        side_by_side = true,
+        layout_config = {
+          preview_height = 0.8,
+        },
+      },
+    },
+    config = function(_, opts)
 
-    require("telescope").load_extension("file_browser")
-
+      require("telescope").load_extension("file_browser")
       require("yanky").setup()
       require("telescope").load_extension("yank_history")
       require("telescope").load_extension("undo")
 
-      end
+    end
   }
 }
