68f97d000be786a7d3f785f11bd6d4afb0490162
Jason Peddle <jason@typh.com>
2023-06-02 22:39:08 -0400

    Scripted auto-commit on change (2023-06-02 22:39:08) by gitwatch.sh

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -20,8 +20,8 @@ return {
           end,
         },
         window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
