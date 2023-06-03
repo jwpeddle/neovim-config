26741032e3307efaa4e505ed5dcbbd37e5fbd548
Jason Peddle <jason@typh.com>
2023-06-02 21:27:36 -0400

    Scripted auto-commit on change (2023-06-02 21:27:36) by gitwatch.sh

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,9 +7,9 @@ return {
       highlight = {
         enable = true,
       },
-    }
+    },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
-    end
+    end,
   }
 }
