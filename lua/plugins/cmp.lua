47ac04634e126f990a4b346bf4c6999d17f02ada
Jason Peddle <jason@typh.com>
2023-06-02 22:39:46 -0400

    Scripted auto-commit on change (2023-06-02 22:39:46) by gitwatch.sh

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,10 +19,7 @@ return {
             require("luasnip").lsp_expand(args.body)
           end,
         },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
+
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -30,6 +27,7 @@ return {
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
+
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
