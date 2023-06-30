9bf79d57aa19f53b8add3702d16af151b22264b9
Jason Peddle <jason@typh.com>
2023-06-30 14:46:14 -0400

    Scripted auto-commit on change (2023-06-30 14:46:14) by gitwatch.sh

---
+++ lua/plugins/lean.lua
@@ -0,0 +1,15 @@
+--lean lsp
+return {
+  {
+    "Julian/lean.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      abbreviations = { builtin = true},
+      lsp = { on_attach = "on_attach"},
+      mappings = true,
+    }
+  }
+}
