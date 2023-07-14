842d7dbb4053fea20b0abbef6f3df3936b57212c
Jason Peddle <jason@typh.com>
2023-07-14 01:41:49 -0400

    Scripted auto-commit on change (2023-07-14 01:41:49) by gitwatch.sh

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -26,6 +26,16 @@ return {
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
           lsp_doc_border = false, -- add a border to hover docs and signature help
         },
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              kind = "",
+              find = "written",
+            },
+            opts = { skip = true },
+          },
+        },
       })
     end,
   }
