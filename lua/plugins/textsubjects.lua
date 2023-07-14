31c785d298dde2079b4ea22e424bf933a37dd4ab
Jason Peddle <jason@typh.com>
2023-07-14 01:27:03 -0400

    Scripted auto-commit on change (2023-07-14 01:27:03) by gitwatch.sh

--- lua/plugins/textsubjects.lua
+++ lua/plugins/textsubjects.lua
@@ -4,13 +4,13 @@ return {
     "RRethy/nvim-treesitter-textsubjects",
     opts = {
       textsubjects = {
-          enable = true,
-          prev_selection = ',', -- (Optional) keymap to select the previous selection
-          keymaps = {
-              ['.'] = 'textsubjects-smart',
-              [';'] = 'textsubjects-container-outer',
-              ['i;'] = 'textsubjects-container-inner',
-          },
+        enable = true,
+        prev_selection = ',', -- (Optional) keymap to select the previous selection
+        keymaps = {
+            ['.'] = 'textsubjects-smart',
+            [';'] = 'textsubjects-container-outer',
+            ['i;'] = 'textsubjects-container-inner',
+        },
       },
     },
   }
