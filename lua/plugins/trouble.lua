33848a76a4895d1fda4be27af6f23451f809a8af
Jason Peddle <jason@typh.com>
2023-07-13 21:27:57 -0400

    Scripted auto-commit on change (2023-07-13 21:27:57) by gitwatch.sh

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -5,6 +5,21 @@ return {
     dependencies = "kyazdani42/nvim-web-devicons",
     opts = {
       auto_close = true,
-    }
+    },
+    config = function(_, opts)
+      local actions = require("telescope.actions")
+      local trouble = require("trouble.providers.telescope")
+
+      local telescope = require("telescope")
+
+      telescope.setup {
+        defaults = {
+          mappings = {
+            i = { ["<c-t>"] = trouble.open_with_trouble },
+            n = { ["<c-t>"] = trouble.open_with_trouble },
+          },
+        },
+      }
+    end,
   }
 }
