12379736296692ce32ec70626e2cfea3d1ff8da1
Jason Peddle <jason@typh.com>
2023-07-14 01:11:38 -0400

    Scripted auto-commit on change (2023-07-14 01:11:38) by gitwatch.sh

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,9 +12,9 @@ return {
         enable = true,
         keymaps = {
           init_selection = '<CR>',
-          scope_incremental = '<CR>',
-          node_incremental = '<TAB>',
-          node_decremental = '<S-TAB>',
+          node_incremental = '<CR>',
+          --node_decremental = '<S-TAB>',
+          --scope_incremental = '<CR>',
         },
       },
     },
