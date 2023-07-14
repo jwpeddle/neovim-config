c21d039bb9a6065943c2457af7fd174adab4e422
Jason Peddle <jason@typh.com>
2023-07-14 01:26:09 -0400

    Scripted auto-commit on change (2023-07-14 01:26:09) by gitwatch.sh

--- lua/plugins/textsubjects.lua
+++ lua/plugins/textsubjects.lua
@@ -1,10 +1,17 @@
 --diagnostics
 return {
   {
-    "folke/trouble.nvim",
-    dependencies = "kyazdani42/nvim-web-devicons",
+    "RRethy/nvim-treesitter-textsubjects",
     opts = {
-      auto_close = true,
+      textsubjects = {
+          enable = true,
+          prev_selection = ',', -- (Optional) keymap to select the previous selection
+          keymaps = {
+              ['.'] = 'textsubjects-smart',
+              [';'] = 'textsubjects-container-outer',
+              ['i;'] = 'textsubjects-container-inner',
+          },
+      },
     },
   }
 }
