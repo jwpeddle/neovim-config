f0c1e1ecb85bb5082ad3837c2d9c199e23c4c8fd
Jason Peddle <jason@typh.com>
2023-07-13 21:40:57 -0400

    Scripted auto-commit on change (2023-07-13 21:40:57) by gitwatch.sh

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -6,21 +6,5 @@ return {
     opts = {
       auto_close = true,
     },
-    config = function(_, opts)
-      require("trouble").setup(opts)
-      local actions = require("telescope.actions")
-      local trouble = require("trouble.providers.telescope")
-
-      local telescope = require("telescope")
-
-      telescope.setup {
-        defaults = {
-          mappings = {
-            i = { ["<c-t>"] = trouble.open_with_trouble },
-            n = { ["<c-t>"] = trouble.open_with_trouble },
-          },
-        },
-      }
-    end,
   }
 }
